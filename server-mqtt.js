70b1e227251a85e3503d7463c257ee1786f03cbf
Oguzhan Alan <oguzhan.alan@compronet.de>
2016-05-02 01:37:42 +0200

    mqtt prepare moca broker

--- server-mqtt.js
+++ server-mqtt.js
@@ -6,7 +6,7 @@ require('./config/init')();
 var config = require('./config/config');
 var mongoose = require('mongoose');
 var path = require('path');
-var socketio = require('socket.io');
+
 /**
  * Main application entry file.
  * Please note that the order of loading is important.
@@ -20,13 +20,13 @@ var db = mongoose.connect(config.db, function(err) {
     }
 });
 
-// Init the express application
 var app = require('./config/express')(db);
 // Globbing model files
 config.getGlobbedFiles('./app/models/**/*.js').forEach(function(modelPath) {
     require(path.resolve(modelPath));
 });
 
-require('./config/ping')(socketio());
+//start the ping monitoring for devices
+require('./config/ping')();
 
 console.log('MQTT application started');
