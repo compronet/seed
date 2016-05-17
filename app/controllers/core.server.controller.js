5eec32c7580574b6ca17baf424708d9b1bf2f085
Oguzhan Alan <oguzhan.alan@compronet.de>
2016-05-17 15:44:55 +0200

    login view initial

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -6,6 +6,7 @@ var config = require('../../config/config');
  * Module dependencies.
  */
 exports.index = function(req, res) {
+	//TODO: render login page on missing auth
 	res.render('index', {
 		user: req.user || null,
 		rootTopic: config.mqtt.rootTopic,
