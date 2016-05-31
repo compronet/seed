af7c9a8fc6c12261a21cb210c053dba9fcf49279
laszlohornyak88 <laszlo.hornyak@compronet.de>
2016-05-31 16:18:55 +0200

    helmet update

--- config/express.js
+++ config/express.js
@@ -108,10 +108,10 @@ module.exports = function(db) {
 	app.use(flash());
 
 	// Use helmet to secure Express headers
-	app.use(helmet.xframe());
+	app.use(helmet.frameguard());
 	app.use(helmet.xssFilter());
-	app.use(helmet.nosniff());
-	app.use(helmet.ienoopen());
+	app.use(helmet.noSniff());
+	app.use(helmet.ieNoOpen());
 	app.disable('x-powered-by');
 
 	// Setting the app router and static folder
