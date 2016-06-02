8606ab344a46ffc3802bc42ac5dc4ebcdadce208
laszlohornyak88 <laszlo.hornyak@compronet.de>
2016-06-02 17:05:29 +0200

    New app modal directly from device view

---
+++ public/modules/apps/controllers/app.create.modal.controller.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	// Apps controller
+	angular.module('apps').controller('AppCreateModalController', ['_', '$scope', '$uibModalInstance', 'Apps',
+		function(_, $scope, $uibModalInstance, Apps) {
+			$scope.app = {};
+
+			$scope.create = function() {
+				var newApp = new(Apps.getRestApi())($scope.app);
+				newApp.$save(
+					function(response) {
+						Apps.notify(response);
+						$uibModalInstance.close(response);
+					},
+
+					function(err) {
+						$scope.error = err;
+					}
+				);
+			};
+
+			$scope.cancel = function() {
+				$uibModalInstance.dismiss('cancel');
+			};
+
+		}
+	]);
+})();
