52456648911f48765100544913a62cf6603924a0
Ricardo Bastos <ricardo.bastos@compronet.de>
2016-06-13 18:04:09 +0200

    Generalize comparison of authenticated data

---
+++ public/modules/core/services/appHelper.client.service.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+	angular.module('core').factory('AppHelper', ['$rootScope', '$resource', 'Authentication', AppHelper]);
+	function AppHelper($rootScope, $resource, Authentication) {
+		var service = {
+			authData: authData
+		};
+
+		return service;
+
+		function authData(dataSet) {
+			var isAuth = false;
+
+			if (typeof dataSet !== 'undefined') {
+				isAuth = angular.equals(dataSet.user._id, Authentication.user._id);
+			}
+			
+			return isAuth;
+		}
+
+	}
+})();
