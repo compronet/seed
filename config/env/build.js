a2b1ec744fd4c2a0200312dcc20b8b22cb4bc35b
Oguzhan Alan <oguzhan.alan@compronet.de>
2016-07-11 18:55:01 +0200

    config env rename added

---
+++ config/env/build.js
@@ -0,0 +1,85 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'compronet SEED',
+		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+		keywords: 'MongoDB, Express, AngularJS, Node.js'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	sessionCookie: {
+		// session expiration is set by default to 24 hours
+		maxAge: 24 * (60 * 60 * 1000),
+
+		// httpOnly flag makes sure the cookie is only accessed
+		// through the HTTP protocol and not JS/browser
+		httpOnly: true,
+
+		// secure cookie should be turned to true to provide additional
+		// layer of security so that the cookie is set only when working
+		// in HTTPS mode.
+		secure: false
+	},
+	sessionKey: 'sessionId',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				'public/lib/metisMenu/dist/metisMenu.min.css',
+				'public/lib/animate.css/animate.min.css',
+				'public/lib/whirl.css/dist/whirl.min.css'
+			],
+			js: [
+				'public/lib/lodash/dist/lodash.min.js',
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/bootstrap/dist/js/bootstrap.min.js',
+				'public/lib/metisMenu/dist/metisMenu.min.js',
+				'public/scripts.js',
+				'public/lib/angular/angular.min.js',
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-cookies/angular-cookies.min.js',
+				'public/lib/angular-translate/angular-translate.min.js',
+				'public/lib/angular-translate-loader-url/angular-translate-loader-url.min.js',
+				'public/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
+				'public/lib/angular-translate-storage-local/angular-translate-storage-local.min.js',
+				'public/lib/angular-translate-storage-cookie/angular-translate-storage-cookie.min.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-touch/angular-touch.min.js',
+				'public/lib/angular-sanitize/angular-sanitize.min.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular-ui-utils/index.js',
+				'public/lib/angular-ui-event/dist/event.min.js',
+                'public/lib/angular-ui-grid/ui-grid.min.js',
+                'public/lib/angular-ui-indeterminate/dist/indeterminate.min.js',
+                'public/lib/angular-ui-mask/dist/mask.min.js',
+                'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+                'public/lib/angular-ui-scroll/dist/ui-scroll.min.js',
+                'public/lib/angular-ui-scrollpoint/dist/scrollpoint.min.js',
+                'public/lib/angular-ui-select/dist/select.min.js',
+                'public/lib/angular-ui-uploader/dist/uploader.min.js',
+                'public/lib/angular-ui-validate/dist/validate.min.js',
+				'public/lib/angular-gravatar/build/angular-gravatar.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/jquery-flot/jquery.flot.js'
+			]
+		},
+		css: [
+			'public/www/css/AdminLTE.min.css',
+			'public/www/css/skins/_all-skins.min.css',
+			'public/modules/**/css/*.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
